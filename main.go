027d3ee3cf1e66c8c9abf17a0f1a5851de7d1d0e
Oskar Thoren <o.thoren@gmail.com>
2012-08-10 13:00:30 -0400

    decouple webrtc.io-go from wsevents

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"code.google.com/p/go.net/websocket"
 	"flag"
-  "fmt"
+	"fmt"
+	"github.com/oskarth/wsevents"
 	"log"
 	"net/http"
 	"text/template"
@@ -13,21 +14,21 @@ var addr = flag.String("addr", ":8080", "http service address")
 var homeTempl = template.Must(template.ParseFiles("index.html"))
 
 func homeHandler(c http.ResponseWriter, req *http.Request) {
-  homeTempl.Execute(c, req.Host)
+	homeTempl.Execute(c, req.Host)
 }
 
 func resourceHandler(c http.ResponseWriter, req *http.Request) {
-  fmt.Print("Serving " + req.URL.Path[1:], "\n")
-  http.ServeFile(c, req, req.URL.Path[1:])
+	fmt.Print("Serving "+req.URL.Path[1:], "\n")
+	http.ServeFile(c, req, req.URL.Path[1:])
 }
 
 func main() {
 	flag.Parse()
-	go h.run()
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/resources/webrtc.io.js", resourceHandler)
-  http.HandleFunc("/resources/style.css", resourceHandler)
-  http.Handle("/ws", websocket.Handler(wsHandler))
+	go wsevents.Run()
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/resources/webrtc.io.js", resourceHandler)
+	http.HandleFunc("/resources/style.css", resourceHandler)
+	http.Handle("/ws", websocket.Handler(wsevents.Handler))
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
